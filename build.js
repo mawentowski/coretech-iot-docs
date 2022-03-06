40052f4cb8e19b56bae674988fb426f657c7ca03
Raymond Wentowski <raymond.wentowski@duke-energy.com>
2022-03-06 11:36:14 -0500

    Further developed build process to include conversion of markdown to html.

--- build.js
+++ build.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const { marked } = require('marked');
+const fse = require('fs-extra');
 
-function deriveTabLabelFromFileName(filename) {
-    const regExpExecArray = /(\S*).html/.exec(filename);
-    const filenameWithoutExtension = regExpExecArray[1];
+function getMarkdownFilenameWithoutExtension(filename) {
+    const regExpExecArray = /(\S*).md/.exec(filename);
+    return regExpExecArray[1];
+}
+
+function deriveTabLabelFromTopicFileName(filename) {
+    const filenameWithoutExtension =
+        getMarkdownFilenameWithoutExtension(filename);
     const filenameSegments = filenameWithoutExtension.split('-');
     const tabLabel = filenameSegments.reduce((accumulator, currentValue) => {
         const firstLetter = currentValue[0].toUpperCase();
@@ -13,22 +19,37 @@ function deriveTabLabelFromFileName(filename) {
     return tabLabel;
 }
 
-const indexFilename = 'index.html';
-let indexHtmlContent = fs.readFileSync(indexFilename, 'utf8');
-const contentTabsOpeningDivTag = '<!--Content tabs before here.-->';
-const indexHtmlContentSegments = indexHtmlContent.split(
-    contentTabsOpeningDivTag
-);
-const tabsFolder = 'tabs/';
-fs.readdirSync(tabsFolder).forEach((filename) => {
-    globalThis.console.log('filename', filename);
-    const tabLabel = deriveTabLabelFromFileName(filename);
-    indexHtmlContentSegments[0] += `<button class="">${tabLabel}</button>\n`;
-});
 const buildFolderName = 'build';
-const compiledIndexPath = `${buildFolderName}/${indexFilename}`;
-fs.unlinkSync(indexFilename);
+const srcFolderName = 'src';
+const topicsFolderName = 'topics';
+const buildTopicsPath = `${buildFolderName}/${topicsFolderName}`;
+
+// Replace build folder and contents with fresh copy of src folder.
+fse.copySync(srcFolderName, buildFolderName, { overwrite: true });
+// Remove copies of topic markdown files.
+fs.rmdirSync(buildTopicsPath, { recursive: true });
+fs.mkdirSync(buildTopicsPath);
+
+const indexFilename = 'index.html';
+let indexContent = fs.readFileSync(`${srcFolderName}/${indexFilename}`, 'utf8');
+const contentTabsInsertPoint = '<!--Content tabs before here.-->';
+const indexContentSegments = indexContent.split(contentTabsInsertPoint);
+const srcTopicsPath = `${srcFolderName}/${topicsFolderName}`;
+fs.readdirSync(`${srcTopicsPath}/`).forEach((filename) => {
+    globalThis.console.log('topic markdown file', filename);
+    const fileContent = fs.readFileSync(`${srcTopicsPath}/${filename}`, 'utf8');
+    const html = marked.parse(fileContent);
+    fs.writeFileSync(
+        `${buildTopicsPath}/${getMarkdownFilenameWithoutExtension(
+            filename
+        )}.html`,
+        html
+    );
+    const tabLabel = deriveTabLabelFromTopicFileName(filename);
+    indexContentSegments[0] += `<button class="">${tabLabel}</button>\n`;
+});
 fs.writeFileSync(
-    compiledIndexPath,
-    indexHtmlContentSegments.join(contentTabsOpeningDivTag)
+    `${buildFolderName}/${indexFilename}`,
+    indexContentSegments.join(contentTabsInsertPoint),
+    { overwrite: true }
 );
