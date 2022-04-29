9af999efa5644b38e6e61076f919582e45d3a5c7
Mark Wentowski <mawentowski@Marks-MacBook-Pro.local>
2022-04-29 11:08:48 +0200

    minor tweaks to .code-tabs and created the method-example style for Request Bodies, responses, etc.

--- src/js/code-tab.js
+++ src/js/code-tab.js
@@ -2,7 +2,7 @@ function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // Get all elements with class="tabcontent dark" and hide them
     tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = 'none';
