2da2528f5dc65c1bfeb54c1502b452b9532674bf
Raymond Wentowski <raymond.wentowski@duke-energy.com>
2022-05-01 12:21:54 -0400

    Tweaks to build process and styling.

--- build/left-nav.js
+++ build/left-nav.js
@@ -129,14 +129,14 @@ function insertLeftNavOptionIntoDistIndexHtml(
     const distIndexHtmlContentSegments =
         distIndexHtmlContent.split(optionsInsertPoint);
     let optionHtml;
+    let leftNavOptionDivClass = 'hidden left-nav-option';
+    if (isTopLevelLeftNavOption)
+        leftNavOptionDivClass = `${leftNavOptionDivClass} top-level`;
     if (isSection) {
         const svgMarkup =
             '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"/></svg>';
-        let leftNavOptionDivClass = 'hidden left-nav-option';
-        if (isTopLevelLeftNavOption)
-            leftNavOptionDivClass = `${leftNavOptionDivClass} top-level`;
         optionHtml = `
-        <div class="${leftNavOptionDivClass}" data-tab-name="${tabName}">
+        <div class="${leftNavOptionDivClass} left-nav-option-with-section" data-tab-name="${tabName}">
         <span class="collapsed left-nav-section-container">
             <span class="svg-container">
                 ${svgMarkup}
@@ -155,7 +155,7 @@ function insertLeftNavOptionIntoDistIndexHtml(
             topicHtmlFileRelativeUrl
         );
         optionHtml = `
-        <div class="left-nav-option hidden" data-tab-name="${tabName}">
+        <div class="${leftNavOptionDivClass}" data-tab-name="${tabName}">
         <button class="left-nav-item plain" data-relative-url="${topicHtmlFileRelativeUrl}">
             ${optionLabel}
         </button>`;
